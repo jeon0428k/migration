66d2db233c190fc85ff98e493f7dc4546e958cbd
jeon0428k <jeon0428k@gmail.com>
2023-02-27 01:24:46 +0900

    Init Commit

--- orm.py
+++ orm.py
@@ -1,13 +1,14 @@
-from sqlalchemy import create_engine, MetaData, Table, select, text, tuple_, not_
+from sqlalchemy import create_engine, MetaData, Table, select, text, tuple_, not_, inspect, insert
 from sqlalchemy.ext.automap import automap_base
 from tabulate import tabulate
+from itertools import groupby
 
 EXCLUDE_TABLES = [
     ('DB1', 'TAB3')
 ]
 
 
-def get_tabs(engine, conn, metadata):
+def get_info_tabs(engine, conn, metadata):
     data = Table('TABLES', metadata, autoload_with=engine)
     cols = [data.c.TABLE_SCHEMA,
             data.c.TABLE_NAME
@@ -24,7 +25,7 @@ def get_tabs(engine, conn, metadata):
     } for row in conn.execute(query)]
 
 
-def get_cols(engine, conn, metadata, diff_tables):
+def get_info_cols(engine, conn, metadata, diff_tables):
     data = Table('COLUMNS', metadata, autoload_with=engine)
     cols = [data.c.TABLE_SCHEMA,
             data.c.TABLE_NAME,
@@ -81,7 +82,7 @@ def dict_diff(src: [], dst: []):
     dst_values = [tuple(d.values()) for d in dst]
     # print(dst_values)
 
-    diff_values = set(dst_values) - set(src_values)
+    diff_values = set(src_values) - set(dst_values)
     # print(diff_values)
 
     keys = tuple(dst[0].keys())
@@ -90,28 +91,92 @@ def dict_diff(src: [], dst: []):
     return result
 
 
-src_engine, src_conn, src_metadata = create_connect(DbMcsConfig, 'information_schema')
-dst_engine, dst_conn, dst_metadata = create_connect(DbWltConfig, 'information_schema')
+src_engine, src_conn, src_metadata = create_connect(DbWltConfig, 'information_schema')
+dst_engine, dst_conn, dst_metadata = create_connect(DbMcsConfig, 'information_schema')
 
-src_result = get_tabs(src_engine, src_conn, src_metadata)
+src_tabs = get_info_tabs(src_engine, src_conn, src_metadata)
 # print(src_result)
 
-dst_result = get_tabs(dst_engine, dst_conn, dst_metadata)
+dst_tabs = get_info_tabs(dst_engine, dst_conn, dst_metadata)
 # print(dst_result)
 
-diff_tables = dict_diff(src_result, dst_result)
+diff_tables = dict_diff(src_tabs, dst_tabs)
 
 print(tabulate(diff_tables, headers='keys', tablefmt='psql', showindex=True))
 
-# if len(diff_tables) > 0:
-#     for row in diff_tables:
-
-src_result = get_cols(src_engine, src_conn, src_metadata, diff_tables)
+src_cols = get_info_cols(src_engine, src_conn, src_metadata, diff_tables)
 # print(src_result)
 
-dst_result = get_cols(dst_engine, dst_conn, dst_metadata, diff_tables)
+dst_cols = get_info_cols(dst_engine, dst_conn, dst_metadata, diff_tables)
 # print(dst_result)
 
-diff_cols = dict_diff(src_result, dst_result)
+diff_cols = dict_diff(src_cols, dst_cols)
 
 print(tabulate(diff_cols, headers='keys', tablefmt='psql', showindex=True))
+
+if len(diff_cols) > 0:
+    print('diff exists !!!')
+    exit()
+
+print(tabulate(src_tabs, headers='keys', tablefmt='psql', showindex=True))
+
+grouped_tabs = {}
+for name, group in groupby(src_tabs, key=lambda tab: tab['TABLE_SCHEMA']):
+    grouped_tabs[name] = list(group)
+print(grouped_tabs)
+
+
+def get_select(engine, conn, metadata, table_name):
+    data = Table(table_name, src_metadata, autoload_with=src_engine)
+    query = select(data)
+    return conn.execute(query)
+
+
+def set_insert(engine, conn, metadata, table_name, new_data):
+    data = Table(table_name, src_metadata, autoload_with=src_engine)
+    query = insert(data).values(**new_data).on_duplicate_key_update(
+        name=insert(data).values.name
+    )
+    print(query)
+    return conn.execute(query)
+
+
+for key in grouped_tabs:
+    tabs = grouped_tabs[key]
+    for index, tab in enumerate(tabs):
+        if index == 0:
+            src_engine, src_conn, src_metadata = create_connect(DbWltConfig, tab['TABLE_SCHEMA'])
+            dst_engine, dst_conn, dst_metadata = create_connect(DbMcsConfig, tab['TABLE_SCHEMA'])
+
+        src_results = get_select(src_engine, src_conn, src_metadata, tab['TABLE_NAME'])
+        dst_results = set_insert(dst_engine, dst_conn, dst_metadata, tab['TABLE_NAME'], src_results)
+        print(dst_results.rowcount)
+
+# for row in src_tabs:
+#     engine, conn, metadata = DB_CONN.get(row['TABLE_SCHEMA'])
+#     data = Table(row['TABLE_NAME'], metadata, autoload_with=engine)
+#     query = select(data)
+#     for d in conn.execute(query):
+#         print(d)
+
+
+# src_engine, src_conn, src_metadata = create_connect(DbMcsConfig, 'DB1')
+# dst_engine, dst_conn, dst_metadata = create_connect(DbWltConfig, 'DB1')
+#
+# inspector = inspect(src_engine)
+# tables = inspector.get_table_names()
+# print(tables)
+#
+# for table in tables:
+#     src_table = Table(table, src_metadata, autoload_with=src_engine)
+#     dst_table = Table(table, dst_metadata, autoload_with=dst_engine)
+#
+#     select_stmt = select(src_table)
+#     print('select_stmt', select_stmt)
+#     data = src_conn.execute(select_stmt).fetchall()
+#     print('data', data)
+#
+#     for row in data:
+#         insert_stmt = insert(dst_table).values(**row)
+#         print('insert_stmt', insert_stmt)
+#         dst_conn.execute(insert_stmt)
